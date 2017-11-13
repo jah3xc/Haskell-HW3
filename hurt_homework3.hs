84db86cc2e31072c1540d62f2c0f4ebb444d7f65
davidemily <daeth5@mail.missouri.edu>
2017-11-13 14:57:27 -0600

    rpnast.hs and prob1

--- hurt_homework3.hs
+++ hurt_homework3.hs
@@ -1,13 +1,23 @@
 {-# OPTIONS_GHC -fwarn-missing-signatures #-}
 {-# OPTIONS_GHC -fno-warn-tabs #-}
-module Homework3 where
 
+module Homework3 where
 import Test.Hspec
 import RPNAST
 
 
-prob1    :: a
-prob1    = undefined
+--prob1    :: a
+--prob1    = undefined
+prob1 :: String -> PExp
+prob1 x = decipher(words x)
+
+decipher :: String -> PExp
+  | [] = []
+  | x == "+" = Plus:(decipher xs)
+  | x == "-" = Minus:(decipher xs)
+  | x == "*" = Mul:(decipher xs)
+  | x == "/" = IntDiv:(decipher xs)
+  | x        = (Val (read x :: Int)):(decipher xs)
 
 prob2    :: a
 prob2    = undefined
